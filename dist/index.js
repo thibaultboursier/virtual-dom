7d7d8667b22b0f6535fe9da735e9d531aa04d878
thibaultboursier <boursier.thibault@gmail.com>
2017-04-14 00:10:00 +0200

    bootstrapping project

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Thibault on 13/04/2017.
+ */
+
+'use strict';
+
+var x = function x() {
+  return 2;
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Thibault on 13/04/2017.
+ */
+
+'use strict';
+
+const x = () => 2;
